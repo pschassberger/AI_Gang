e26c0b5af4b3de3686e445a4a297cc72f161d9b6
pschassberger <pschassberger@gmail.com>
2020-10-10 17:38:41 -0600

    Added game window

--- game.py
+++ game.py
@@ -1,1 +1,22 @@
-# write game
+import tkinter as tk
+
+
+class Connect4(tk.Canvas):
+    def __init__(self):
+        super().__init__(
+            width=1080, height=720, background="black", highlightthickness=0
+        )
+
+        self.pack()
+
+
+root = tk.Tk()
+root.title("AI Connect 4")
+root.resizable(False, False)
+root.tk.call("tk", "scaling", 4.0)
+
+board = Connect4()
+
+root.mainloop()
+
+
